5774ab80e4cb2f09080b6bc18412dafd2c1c45ab
trzynogazyrafa <97914293+trzynogazyrafa@users.noreply.github.com>
2023-02-19 10:24:02 +0100

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+18o@d%)3xn&j+ye8oq%_niry=xlgbztasrwm)uh15&7j^(fw='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
